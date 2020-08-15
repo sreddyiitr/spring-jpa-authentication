b468d97f0e3ad44bc80478c5e4cd4c9ed968b2c6
Sunil Kothireddy <Sunil@sunils-mbp.lan>
2020-08-15 09:20:37 -0500

    initial

---
+++ src/main/java/com/spring/security/jpa/authentication/SpringJpaAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.jpa.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/security/jpa/authentication/SpringJpaAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.jpa.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
